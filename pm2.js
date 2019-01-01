873786fd3cc6a33295f5e1bceef8696355ad8c1a
Oyewole Samuel <soyewole@swifta.com>
2019-01-01 09:36:14 +0100

    update pm config

--- pm2.js
+++ pm2.js
@@ -4,6 +4,8 @@ module.exports = {
             name: "meatify",
             script: "./bin/www",
             watch: true,
+            instances: 0,
+            exec_mode   : "cluster",
             env: {
                 "PORT": 3000,
                 "NODE_ENV": "development"
