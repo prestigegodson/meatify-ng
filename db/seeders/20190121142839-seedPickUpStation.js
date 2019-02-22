95adf62462ffb1c158a1c1d08dc97a4534b3ac0c
Oyewole Samuel <soyewole@swifta.com>
2019-02-22 19:56:16 +0100

    update datetime format for seedpickupstation

--- db/seeders/20190121142839-seedPickUpStation.js
+++ db/seeders/20190121142839-seedPickUpStation.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const uuid = require("uuid/v4");
+
 module.exports = {
   up: (queryInterface, Sequelize) => {
     /*
@@ -7,15 +9,26 @@ module.exports = {
       Return a promise to correctly handle asynchronicity.
     */
 
-    return queryInterface.bulkInsert('pickup_stations', [{
-      address: "8 Abiodun Aina,Street, Abule Tailor B/stop, Abule Egba",
-      landmark: "Abule Tailor",
-      phone: "09063057476, 09037775481",
-      workinghours: "Victoria Island",
-      state_id: 25,
-      city_id: 497
-    }], {});
-    
+    return queryInterface.bulkInsert('pickup_stations', [
+      {
+        id: uuid(),
+        address: "8 Abiodun Aina,Street, Abule Tailor B/stop, Abule Egba",
+        landmark: "Abule Tailor",
+        phone: "09063057476, 09037775481",
+        workinghours: "Mon-Fri, 8am-8pm; Saturday, 9am-5pm; Public Holidays, 9am-5pm",
+        state_id: 25,
+        city_id: 497
+      },
+      {
+        id: uuid(),
+        address: "19 Association Rd, Veora Estate, Arepo, Ogun State",
+        landmark: "Punch Newspaper",
+        phone: "07063317344, 09037775481",
+        workinghours: "Mon-Fri, 8am-8pm; Saturday, 9am-5pm; Public Holidays, 9am-5pm",
+        state_id: 25,
+        city_id: 497
+      }], {});
+
   },
 
   down: (queryInterface, Sequelize) => {
