a72ab9e0f0b7093988fbc9a3745bd879022bb867
Oyewole S. Abayomi <oyewoleabayomi@gmail.com>
2019-06-10 14:16:33 +0100

    update

--- controllers/TransactionController.js
+++ controllers/TransactionController.js
@@ -65,7 +65,7 @@ module.exports = {
         try {
             paystack.transaction.initialize({
                 email: customer.email,
-                amount: amount,
+                amount: amount * 100,
                 reference: uuid(),
                 channels: ['card'],
                 metadata: {
